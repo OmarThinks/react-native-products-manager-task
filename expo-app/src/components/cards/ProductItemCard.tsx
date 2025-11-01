04c47ced316d2143cc0a1f627982aaf9fe5af257
Omar Magdy <24939461+OmarThinks@users.noreply.github.com>
2025-11-01 13:27:54 +0200

    feat: displayed image in card

--- expo-app/src/components/cards/ProductItemCard.tsx
+++ expo-app/src/components/cards/ProductItemCard.tsx
@@ -1,48 +1,55 @@
-import { View, Text, TouchableOpacity } from "react-native";
-import React from "react";
-import { ProductType } from "@/src/types/ProductType";
 import { useColors } from "@/src/redux/slices/themeSlice/colorsHooks";
+import { ProductType } from "@/src/types/ProductType";
+import { Image } from "expo-image";
 import { router } from "expo-router";
+import React from "react";
+import { Text, TouchableOpacity, View } from "react-native";
 
 const ProductItemCard = ({ product }: { product: ProductType }) => {
   const colors = useColors();
 
   return (
     <TouchableOpacity
-      className=" self-stretch rounded-[8px] border px-2 py-3"
+      className=" self-stretch rounded-[8px] border px-3 py-3 flex-row gap-2"
       style={{
         borderColor: colors.text,
       }}
       onPress={() => router.push(`/productDetails/${product.id}`)}
     >
-      <Text
-        style={{ color: colors.primary }}
-        className=" text-[20px] font-bold"
-        numberOfLines={1}
-      >
-        {product.title}
-      </Text>
-      <Text style={{ color: colors.text }} numberOfLines={1}>
-        {product.description}
-      </Text>
-      <Text
-        style={{ color: colors.text }}
-        numberOfLines={1}
-        className=" font-semibold"
-      >
-        $
-        <Text style={{ color: colors.secondary }} numberOfLines={1}>
-          {product.price}
+      <Image
+        style={{ width: 100, height: 100, borderRadius: 8 }}
+        source={{ uri: product.image }}
+      />
+      <View className=" flex-1 self-stretch">
+        <Text
+          style={{ color: colors.primary }}
+          className=" text-[20px] font-bold"
+          numberOfLines={1}
+        >
+          {product.title}
         </Text>
-      </Text>
-      <Text style={{ color: colors.text }} numberOfLines={1}>
-        ID: {product.id}
-      </Text>
-      {product.tags.length > 0 && (
         <Text style={{ color: colors.text }} numberOfLines={1}>
-          Tags: {product.tags.join(", ")}
+          {product.description}
         </Text>
-      )}
+        <Text
+          style={{ color: colors.text }}
+          numberOfLines={1}
+          className=" font-semibold"
+        >
+          $
+          <Text style={{ color: colors.secondary }} numberOfLines={1}>
+            {product.price}
+          </Text>
+        </Text>
+        <Text style={{ color: colors.text }} numberOfLines={1}>
+          ID: {product.id}
+        </Text>
+        {product.tags.length > 0 && (
+          <Text style={{ color: colors.text }} numberOfLines={1}>
+            Tags: {product.tags.join(", ")}
+          </Text>
+        )}
+      </View>
     </TouchableOpacity>
   );
 };
