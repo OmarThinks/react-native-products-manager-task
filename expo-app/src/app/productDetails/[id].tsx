56a6b686ee07744313844f6a48a9f407771ed80e
Omar Magdy <24939461+OmarThinks@users.noreply.github.com>
2025-11-01 13:31:13 +0200

    feat: displayed image at product details screen

--- expo-app/src/app/productDetails/[id].tsx
+++ expo-app/src/app/productDetails/[id].tsx
@@ -3,6 +3,7 @@ import React from "react";
 import { useLocalSearchParams, useGlobalSearchParams, Link } from "expo-router";
 import { useProducts } from "@/src/redux/slices/productsSlice/productsHooks";
 import { useColors } from "@/src/redux/slices/themeSlice/colorsHooks";
+import { Image } from "expo-image";
 
 const ProductDetails = () => {
   const { id }: { id: string } = useLocalSearchParams();
@@ -17,9 +18,22 @@ const ProductDetails = () => {
       className=" px-2 py-4 flex-1 self-stretch"
       style={{ backgroundColor: colors.background }}
     >
+      <Image
+        style={{
+          borderRadius: 8,
+          alignSelf: "stretch",
+          width: "100%",
+          aspectRatio: 1,
+        }}
+        source={{ uri: product?.image }}
+      />
+
       <Text style={{ color: colors.text }}>{product?.title}</Text>
-      <Text style={{ color: colors.text }}>{product?.description}</Text>
-      <Text style={{ color: colors.text }}>{product?.price}</Text>
+      <Text style={{ color: colors.text }}>
+        Description: {product?.description}
+      </Text>
+      <Text style={{ color: colors.text }}>Price: ${product?.price}</Text>
+      <Text style={{ color: colors.text }}>ID: {product?.id}</Text>
     </View>
   );
 };
